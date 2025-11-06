60a9981cfbd11839b2ef50886b125dd2609c1e0e
aleks <darklissik.darklissik@gmail.com>
2025-11-06 20:04:22 +0300

    add Rock

---
+++ src/main/java/ru/simulation/app/Rock.java
@@ -0,0 +1,12 @@
+package ru.simulation.app;
+
+public class Rock extends Entity {
+
+    public Rock(Coords coords) {
+        this.icon = "\uD83E\uDEA8";
+        this.coords = coords;
+        this.name = 'r';
+    }
+
+
+}
