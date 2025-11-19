c88ee52a9cd12928143001f9f4dacfaeb387bf1a
aleksLiss <darklissik.darklissik@gmail.com>
2025-11-20 00:45:52 +0300

    add new Model Icon.java

---
+++ src/main/java/ru/simulation/app/model/Icon.java
@@ -0,0 +1,18 @@
+package ru.simulation.app.model;
+
+public class Icon {
+
+    private String icon;
+
+    public Icon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
