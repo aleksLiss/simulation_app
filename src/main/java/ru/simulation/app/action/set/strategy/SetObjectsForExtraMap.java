25c4fe496e1c8f3b74b4c877b1e8f95b8e10db5a
aleksLiss <darklissik.darklissik@gmail.com>
2025-11-20 00:54:23 +0300

    add class SetObjectsForExtraMap.java

---
+++ src/main/java/ru/simulation/app/action/set/strategy/SetObjectsForExtraMap.java
@@ -0,0 +1,43 @@
+package ru.simulation.app.action.set.strategy;
+
+import ru.simulation.app.map.SimulationMap;
+import ru.simulation.app.model.*;
+import ru.simulation.app.utils.RandomCoordinates;
+
+public class SetObjectsForExtraMap implements SetObjects {
+
+    private SimulationMap simulationMap;
+
+    public SetObjectsForExtraMap(SimulationMap simulationMap) {
+        this.simulationMap = simulationMap;
+    }
+
+    @Override
+    public void setObjects(int size) {
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                simulationMap.putIntoMap(
+                        RandomCoordinates.getRandomCoordinate(size), new Rock()
+                );
+                simulationMap.putIntoMap(
+                        RandomCoordinates.getRandomCoordinate(size), new Tree()
+                );
+                simulationMap.putIntoMap(
+                        RandomCoordinates.getRandomCoordinate(size), new Herbivore()
+                );
+
+            }
+            if (i % 2 != 0) {
+                simulationMap.putIntoMap(
+                        RandomCoordinates.getRandomCoordinate(size), new Grass()
+                );
+                simulationMap.putIntoMap(
+                        RandomCoordinates.getRandomCoordinate(size), new Grass()
+                );
+            }
+            simulationMap.putIntoMap(
+                    RandomCoordinates.getRandomCoordinate(size), new Predator()
+            );
+        }
+    }
+}
