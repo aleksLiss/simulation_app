4877d2aad962419f01c5c1a544b66e49236b51f1
aleksLiss <darklissik.darklissik@gmail.com>
2025-11-20 00:45:35 +0300

    add new Model Grass.java

--- src/main/java/ru/simulation/app/Grass.java
+++
@@ -1,9 +0,0 @@
-package ru.simulation.app;
-
-public class Grass extends Entity {
-    public Grass(Coords coords) {
-        this.icon = "\uD83C\uDF3F";
-        this.coords = coords;
-        this.name = 'g';
-    }
-}
---
+++ src/main/java/ru/simulation/app/model/Grass.java
@@ -0,0 +1,8 @@
+package ru.simulation.app.model;
+
+public class Grass extends Entity {
+    public Grass() {
+        this.icon = new Icon("\uD83C\uDF3F");
+        this.name = 'g';
+    }
+}
